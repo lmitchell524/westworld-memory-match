342905e8352efb8d1bc4dbe378854b8cf1862e97
lmitchell524 <lmitchell524@gmail.com>
2018-01-29 21:03:21 -0600

    worked on handleclick function and added images to image directory

--- src/data/data.js
+++ src/data/data.js
@@ -1,92 +1,78 @@
+import bernard from '../assets/images/bernard.jpg';
+import clementine from '../assets/images/clementine.jpg';
+
+
 export default [
     {
         flipped: false,
-        clickable: true,
-        color: 'red'
+        color: bernard
     },
     {
         flipped: false,
-        clickable: true,
         color: 'blue'
     },
     {
         flipped: false,
-        clickable: true,
         color: 'green'
     },
     {
         flipped: false,
-        clickable: true,
         color: 'yellow'
     },
     {
         flipped: false,
-        clickable: true,
         color: 'orange'
     },
     {
         flipped: false,
-        clickable: true,
         color: 'purple'
     },
     {
         flipped: false,
-        clickable: true,
         color: 'black'
     },
     {
         flipped: false,
-        clickable: true,
         color: 'grey'
     },
     {
         flipped: false,
-        clickable: true,
         color: 'skyblue'
     },
     {
         flipped: false,
-        clickable: true,
         color: 'red'
     },
     {
         flipped: false,
-        clickable: true,
         color: 'blue'
     },
     {
         flipped: false,
-        clickable: true,
         color: 'green'
     },
     {
         flipped: false,
-        clickable: true,
         color: 'yellow'
     },
     {
         flipped: false,
-        clickable: true,
         color: 'orange'
     },
     {
         flipped: false,
-        clickable: true,
         color: 'purple'
     },
     {
         flipped: false,
-        clickable: true,
         color: 'black'
     },
     {
         flipped: false,
-        clickable: true,
         color: 'grey'
     },
     {
         flipped: false,
-        clickable: true,
         color: 'skyblue'
     }
 ];
