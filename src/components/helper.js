a56fc598212339e501752f8eb2067082a2634176
lmitchell524 <lmitchell524@gmail.com>
2018-02-02 09:50:32 -0800

    css changes

--- src/components/helper.js
+++ src/components/helper.js
@@ -1,3 +1,5 @@
+import '../assets/css/app.css';
+
 export function doubleArray(array, times = 2){
 
     const newArray = [];
@@ -11,4 +13,29 @@ export function doubleArray(array, times = 2){
         }
     }
     return newArray;
+}
+
+export function transition(){
+  var timer = null;
+  var target = document.querySelector('.iris');
+  var irisPercent = 99.99;
+  var irisDeltaPerSecond = 33.3;
+  var transitionTime = 3000;
+  var timePerInterval = 10;
+  var intervalCount = transitionTime / timePerInterval;
+  var irisDeltaPerInterval = irisDeltaPerSecond*(transitionTime/1000) / intervalCount;
+  var remainingIntervalCount = intervalCount;
+
+  target.style.display = 'block';
+
+  timer = setInterval(incrementTransition, timePerInterval);
+  function incrementTransition(){
+    irisPercent -= irisDeltaPerInterval;
+    //console.log(irisPercent);
+    target.style.background = `radial-gradient(transparent ${irisPercent}%, black ${irisPercent+.001}%, black 100%)`;
+    remainingIntervalCount--;
+    if(!remainingIntervalCount){
+      clearInterval(timer);
+    }
+  }
 }
