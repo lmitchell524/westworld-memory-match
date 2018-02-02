6ce26ed63beb2f164d2dd681f11202bf480976ce
lmitchell524 <lmitchell524@gmail.com>
2018-02-02 11:49:38 -0800

    added logos for level 2 and added level to state

--- src/data/card-images1.js
+++ src/data/card-images1.js
@@ -1,12 +1,12 @@
-import bernard from '../assets/images/bernard.jpg';
-import clementine from '../assets/images/clementine.jpg';
-import doloresBlue from '../assets/images/dolores_blue.jpg';
-import elsie from '../assets/images/elsie.jpg';
-import ford from '../assets/images/ford.jpg';
-import maeve from '../assets/images/maeve.png';
-import manInBlack from '../assets/images/man_in_black.jpg';
-import peterAbernathy from '../assets/images/peter_abernathy.jpg';
-import teddy from '../assets/images/teddy.jpg';
+import bernard from '../assets/images/level1/bernard.jpg';
+import clementine from '../assets/images/level1/clementine.jpg';
+import doloresBlue from '../assets/images/level1/dolores_blue.jpg';
+import elsie from '../assets/images/level1/elsie.jpg';
+import ford from '../assets/images/level1/ford.jpg';
+import maeve from '../assets/images/level1/maeve.png';
+import manInBlack from '../assets/images/level1/man_in_black.jpg';
+import peterAbernathy from '../assets/images/level1/peter_abernathy.jpg';
+import teddy from '../assets/images/level1/teddy.jpg';
 
 export default [
     bernard,
